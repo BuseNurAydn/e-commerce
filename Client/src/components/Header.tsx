32d320bc4d01231ba56fb3d94bd0ca8890c88624
BuseNurAydn <83844222+BuseNurAydn@users.noreply.github.com>
2025-02-26 12:54:23 +0300

    Header düzenlendi

--- Client/src/components/Header.tsx
+++ Client/src/components/Header.tsx
@@ -1,16 +1,52 @@
-import { AppBar, Toolbar, Typography, Container } from '@mui/material';
 
+import { ShoppingCart } from '@mui/icons-material';
+import { AppBar, Toolbar, Typography, List, ListItem, Container, Badge, Box, IconButton } from '@mui/material';
+import { NavLink } from 'react-router';
+
+const links = [
+  { title: "Home", to: "/" },
+  { title: "About", to: "/about" },
+  { title: "Catalog", to: "/catalog" },
+  { title: "Contact", to: "/contact" },
+
+]
+const navStyles = {
+  color: 'inherit',
+  textDecoration: 'none',
+  "&:hover": {
+    color: "secondary.light"
+  },
+  "&.active": {
+    color: "secondary.light",
+  }
+}
 
 const Header = (props: any) => {
   return (
     <AppBar position="static" sx={{ mb: 4 }} color="secondary">
+      <Toolbar sx={{display:'flex', justifyContent:'space-between'}}>
+   
+         <Box sx={{ display: "flex", alignItems: "center"}}>
+            <Typography variant="h6"> E-commerce </Typography>
+         </Box>
 
-      <Toolbar>
-        <Container>
-          <Typography variant="h6"> E-commerce </Typography>
-        </Container>
+        <Box sx={{ display: "flex", alignItems: "center"}}>
+          <List sx={{ display: "flex" }}>
+            {links.map(link =>     //NavLink : sayfa yenilenmeden geçiş yapmak için
+              <ListItem component={NavLink} to={link.to} sx={navStyles} > {link.title} </ListItem>
+            )}
+          </List>
+        </Box>
+
+        <Box>
+          <IconButton size='large' color='inherit' edge='start'>
+            <Badge badgeContent="2" color="primary">
+              <ShoppingCart />
+            </Badge>
+          </IconButton>
+        </Box>
+       
       </Toolbar>
-
     </AppBar>
   )
 }
